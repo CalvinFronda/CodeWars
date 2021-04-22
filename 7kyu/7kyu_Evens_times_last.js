5f6b5517a0d25fca2a0ddffebda77af8279027ab
Calvin Fronda <cfronda209@gmail.com>
2021-04-22 12:01:54 -0700

    feat():evens times last

---
+++ 7kyu/7kyu_Evens_times_last.js
@@ -0,0 +1,16 @@
+/*
+Given a sequence of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
+
+If the sequence is empty, you should return 0.
+*/
+
+function evenLast(numbers) {
+    if (numbers.length === 0) return 0
+    let num = 0
+    for (let i = 0; i < numbers.length; i++) {
+        if (i % 2 === 0) {
+            num = num + numbers[i];
+        }
+    }
+    return numbers[numbers.length - 1] * num
+}
