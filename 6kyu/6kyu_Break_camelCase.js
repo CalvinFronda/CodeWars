7e5f8d1080d523a23bb89cb9dbb7dad4f04a565f
Calvin Fronda <cfronda209@gmail.com>
2021-05-16 17:56:50 -0700

    feat() :break camelCase

---
+++ 6kyu/6kyu_Break_camelCase.js
@@ -0,0 +1,20 @@
+/**
+ * Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+ */
+
+
+// complete the function
+function solution(string) {
+    return string.split('').map((letter) =>
+     letter === letter.toUpperCase() ? 
+     (' ' + letter) : 
+     letter ).join('');
+}
+
+
+console.log(solution('camelCase'))
