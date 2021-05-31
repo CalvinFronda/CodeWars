cd7191ccf2cab815876ab6b23165c60b92da1a9c
Calvin Fronda <cfronda209@gmail.com>
2021-05-31 12:39:36 -0700

    8kyu remove exclamation marks

---
+++ 8kyu/8kyu_Remove_exclamation_marks.js
@@ -0,0 +1,13 @@
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    let result = [];
+    for(let i = 0; i < s.length; i++){
+        if(s[i] !== '!'){
+            result.push(s[i])
+        }
+    }
+    return result.join('');
+}
+
+console.log(removeExclamationMarks("Hello World!!"))
