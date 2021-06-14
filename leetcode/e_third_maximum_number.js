4499f82322ab46fd96752a869bcef5006fed75a5
Calvin Fronda <cfronda209@gmail.com>
2021-06-14 11:49:33 -0700

    feat: third maximum number

---
+++ leetcode/e_third_maximum_number.js
@@ -0,0 +1,54 @@
+/**
+ * Given integer array nums, return the third maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1]
+Output: 1
+Explanation: The third maximum is 1.
+Example 2:
+
+Input: nums = [1,2]
+Output: 2
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+ 
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function (nums) {
+    let newNums = [...new Set(nums)];
+
+    let max = null;
+    let second_max = null;
+    let third_max = null;
+
+    newNums.forEach((num) => {
+        if (max === null || num > max) {
+            third_max = second_max;
+            second_max = max;
+            max = num;
+        } else if (second_max === null || num > second_max) {
+            third_max = second_max;
+            second_max = num;
+        } else if (third_max === null || num > third_max) {
+            third_max = num;
+        }
+    });
+
+    if (third_max == null) {
+        return max;
+    }
+
+    return third_max;
+};
